dc83e0773e34faa8de6755135053ddf1b02f16ec
florinadml <florinapidilis@gmail.com>
2025-09-09 13:13:37 +0800

    update

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -3,34 +3,30 @@ base_salary = float(input())
 overtime_hours = int(input())
 tax_status = input()
 
-# TODO your code here
-tax_rate = 0
-
-overtime_pay = 35 * overtime_hours
-total_income = overtime_pay * base_salary
+# TODO your code her
 
 if tax_status == "Single" and base_salary >= 5000 :
     tax_rate = 0.22
 elif tax_status == "Single" and base_salary <= 5000 :
-    tax_rate == 0.18
+    tax_rate = 0.18
 elif tax_status == "Married" and base_salary >= 6000 :
-    tax_rate == 0.20
+    tax_rate = 0.20
 elif tax_status == "Married" and base_salary <= 6000 :
-    tax_rate == 0.15
+    tax_rate = 0.15
 elif tax_status == "Head" and base_salary >= 5500 :
-    tax_rate == 0.25
- else:
-     tax_rate == 0.19
+    tax_rate = 0.25
 else:
-    tax_rate == 0
+    tax_rate = 0.19
 
-after_tax = base_salary -  (base_salary * tax_rate)
+overtime_pay = overtime_hours * 35
+total_salary = base_salary + overtime_pay
+tax_amount = total_salary *  tax_rate
+after_tax = total_salary - tax_amount
 
-epf = base_salary * 0.11
-sosco = base_salary * 0.005
-
-net_salary = after_tax - (epf + sosco)
+deduction = after_tax * (0.11 + 0.005)
+total_deduction = tax_amount + deduction
+net_salary = total_salary - total_deduction
 
 print(employee_name)
-print(tax_rate)
+print(f"{tax_rate*100:.0f}%")
 print(f"{net_salary:.2f}")
