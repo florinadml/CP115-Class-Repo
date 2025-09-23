a810b5a9d4aa3ed118121697cb12e4dc46417e09
florinadml <florinapidilis@gmail.com>
2025-09-23 12:22:16 +0800

    update

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -1,8 +1,23 @@
 position = input()
 overtime_hours = int(input())
 is_weekend = input()
+hourly_rate = 0
+overtime_pay = 0
+# Determine base hours rate
+if position == "Manager":
+    hourly_rate = 35
+elif position == "Supervisor":
+    hourly_rate = 25
+else:
+    position == "Staff"
+    hourly_rate = 18
 
-# Your code here
+if is_weekend == "Yes":
+    overtime_pay = (1.5 * hourly_rate  * overtime_hours ) + (overtime_hours * 5 )
+else:
+    overtime_pay = (1.5 * hourly_rate  * overtime_hours )
+
+total_pay = overtime_pay 
 
 print(hourly_rate)
 print(overtime_pay)
--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -1,7 +1,23 @@
 age = int(input())
 accident_count = int(input())
+base_premium = 0
 
-# Your code here
+# Determine base premiums
+if age < 25 :
+    base_premium = 2400
+elif age >= 25 and age <= 50 :
+    base_premium = 1800
+else: 
+    age > 50 
+    base_premium = 2000
+
+if accident_count == 0 :
+    penalty = 0 
+elif accident_count == 1 or accident_count == 2 :
+    penalty = 300
+else:
+    accident_count >= 3
+    penalty = 600
 
 print(base_premium)
 print(final_premium)
