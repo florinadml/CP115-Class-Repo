9459ef6a7fc8233c96fb430ec722f8de8c732c58
florinadml <florinapidilis@gmail.com>
2025-09-30 13:03:42 +0800

    update

--- labs/lab11/exercise1/exercise1.py
+++ labs/lab11/exercise1/exercise1.py
@@ -1,7 +1,17 @@
 num_rounds = int(input())
+final_score = 0
 
-# TODO: Your code here
-# Use input() inside the loop to get each round's score
+rounds_processed = 0 
+
+for i in range(num_rounds):
+    round_score = float(input())
+
+    if round_score > 100:
+        bonus = 0.20 * round_score
+        round_score += bonus
+
+    final_score += round_score
+    rounds_processed += 1
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
